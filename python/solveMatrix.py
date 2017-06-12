c3b937573016bf33a0e29d7634370b32f212d859
nicoperillo <n.perillo@campus.unimib.it>
2017-06-12 19:22:50 +0200

    Python code working

--- python/solveMatrix.py
+++ python/solveMatrix.py
@@ -1,3 +1,10 @@
+'''
+Created on 18 April 2017
+Project: solveMatrix
+Class: Metodi del calcolo scientifico
+@author: Nico
+'''
+    
 def solve(*args):
     import scipy.io   
     import pylab
@@ -7,45 +14,49 @@ def solve(*args):
     firstTime = []
     secondErrors = []
     thirdRAM = []
+    
+    #Carico path corrente
+    os.chdir(os.getcwd())
             
-    #Importa tutti i file MTX e le relative info
-    os.chdir
-    mat1 = scipy.io.mmread('../SparseMatrices-MM/ns3Da.mtx')
-    mat1info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat2 = scipy.io.mmread('../SparseMatrices-MM/poisson2D.mtx')
-    mat2info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat3 = scipy.io.mmread('../SparseMatrices-MM/poisson3Da.mtx')
-    mat3info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat4 = scipy.io.mmread('../SparseMatrices-MM/poisson3Db.mtx')
-    mat4info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat5 = scipy.io.mmread('../SparseMatrices-MM/problem1.mtx')
-    mat5info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat6 = scipy.io.mmread('../SparseMatrices-MM/sme3Da.mtx')
-    mat6info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat7 = scipy.io.mmread('../SparseMatrices-MM/sme3Db.mtx')
-    mat7info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-    mat8 = scipy.io.mmread('../SparseMatrices-MM/sme3Dc.mtx')
-    mat8info = scipy.io.mminfo('../SparseMatrices-MM/ns3Da.mtx')
-       
+    #Importa tutti i file MAT
+    print("Reading input...")
+    mat1 = scipy.io.loadmat('../matrices/mat/one20414.mat')
+    mat2 = scipy.io.loadmat('../matrices/mat/two367.mat')
+    mat3 = scipy.io.loadmat('../matrices/mat/three13514.mat')
+    mat4 = scipy.io.loadmat('../matrices/mat/four85623.mat')
+    mat5 = scipy.io.loadmat('../matrices/mat/five415.mat')
+    mat6 = scipy.io.loadmat('../matrices/mat/six12504.mat')
+    mat7 = scipy.io.loadmat('../matrices/mat/seven29067.mat')
+    mat8 = scipy.io.loadmat('../matrices/mat/eigth42930.mat')
+    
+    #Salva tutte le matrici dai file MAT
+    matrix1 = mat1['A']
+    matrix2 = mat2['A'] 
+    matrix3 = mat3['A']
+    matrix4 = mat4['A'] 
+    matrix5 = mat5['A']
+    matrix6 = mat6['A'] 
+    matrix7 = mat7['A']
+    matrix8 = mat8['A']
+    
     #Lista di matrici
-    matrices = [mat1,mat2,mat3,mat4,mat5,mat6,mat7,mat8]
-    matricesInfos = [mat1info,mat2info,mat3info,mat4info,mat5info,mat6info,mat7info,mat8info]
+    matrices = [matrix1, matrix2, matrix3, matrix4, matrix5, matrix6, matrix7, matrix8]
+    
+    #Argomenti opzionali
+    optionMatrices = [];
+    for count, thing in enumerate(args):  #count è il contatore, thing è l'oggetto corrente  
+        temp = scipy.io.loadmat(thing)
+        optionMatrices.append(temp['A'])
     
     #Computazione
     numb=len(args)    
     
     if(numb>0):
-        #Argomenti opzionali
-        optionMatrices = []
-        optionMatricesInfos = []
-        for count, thing in enumerate(args):  #count è il contatore, thing è l'oggetto corrente  
-            temp = scipy.io.mmread(thing)
-            tempInfo = scipy.io.mminfo(thing)
-            optionMatrices.append(temp)
-            optionMatricesInfos.append(tempInfo)
-        firstTime, secondErrors, thirdRAM = mainComput(optionMatrices,optionMatricesInfos,firstTime,secondErrors,thirdRAM)
+        print("\nUsing optional matrices...\n")
+        firstTime, secondErrors, thirdRAM = mainComput(optionMatrices,firstTime,secondErrors,thirdRAM)
     else:
-        firstTime, secondErrors, thirdRAM = mainComput(matrices,matricesInfos,firstTime,secondErrors,thirdRAM)
+        print("\nUsing the 8 FEMLAB matrices...\n")
+        firstTime, secondErrors, thirdRAM = mainComput(matrices,firstTime,secondErrors,thirdRAM)
     
     #Ordino valori in base alla colonna della dimensione
     firstTime = sorted(firstTime,key=lambda x: (x[0],x[1]))
@@ -53,25 +64,26 @@ def solve(*args):
     thirdRAM = sorted(thirdRAM,key=lambda x: (x[0],x[1]))
         
     #Calcolo primo grafico
+    print("\nPlotting results...")
     pylab.figure(1)
     pylab.title('Size and elapsed time')
     pylab.xlabel('Matrix dimension')
-    pylab.ylabel('Time (sec)')
+    pylab.ylabel('Time - seconds')
     pylab.plot([i[0] for i in firstTime], [j[1] for j in firstTime], marker='.', alpha=1, color='b')
     
     #calcolo secondo grafico
     pylab.figure(2)
     pylab.title('Size and errors - logarithmic scale')
     pylab.xlabel('Matrix dimension')
-    pylab.ylabel('Error')	
+    pylab.ylabel('Relative error')	
     pylab.plot([i[0] for i in secondErrors], [j[1] for j in secondErrors], marker='.', alpha=1, color='b')
     pylab.yscale('log')
 
     #Calcolo terzo grafico
     pylab.figure(3)
-    pylab.title('Size and RAM - standard plot')
+    pylab.title('Size and RAM')
     pylab.xlabel('Matrix dimension')
-    pylab.ylabel('RAM used (K)')
+    pylab.ylabel('RAM usage (K)')
     pylab.plot([i[0] for i in thirdRAM], [j[1] for j in thirdRAM], marker='.', alpha=1, color='b')
     
     #Stampo tutti i grafici
@@ -80,27 +92,23 @@ def solve(*args):
     
     
     
-def mainComput(matrices,matricesInfos,firstTime,secondErrors,thirdRAM):    
+def mainComput(matrix,firstTime,secondErrors,thirdRAM):    
     import psutil
     import numpy
-    from scikits.umfpack import spsolve#, splu
+    from scikits.umfpack import spsolve
     import time
     import os
    
-    for i in range(0,len(matrices)):  
-        print('Working on matrix numb ',i+1)
-        
-        #Ottengo matrice i-esima
-        print(matrices[i])
-        print(matrices[i][0])
-        sizeM = matricesInfos[i][0]
-        print(sizeM)
-        A = numpy.zeros((sizeM,sizeM))
-        #for indrow in range(0,sizeM-1):
-        #   for indcol in range(0,sizeM-1):
-        #      print(8)
+    for i in range(0,len(matrix)):  
                 
-        #Memoria RAM usata da python
+        #Ottengo matrice i-esima       
+        A = matrix[i]
+        sizeM = numpy.shape(A)
+        print('Processing matrix n',i+1,', size',sizeM)
+        sizeM = sizeM[0]
+        
+        #Memoria RAM usata da python e tempo attuale
+        ts = time.time()
         process = psutil.Process(os.getpid())    
         firstRAM = process.memory_info().rss
         
@@ -110,11 +118,10 @@ def mainComput(matrices,matricesInfos,firstTime,secondErrors,thirdRAM):
         #Calcolo right-hand side
         b = A * xe
         
-        #Calcolo matrice e tempo di calcolo
-        t = time.time()
-        #lu = splu(A)
+        #Calcolo matrice e tempo di calcolo        
         x = spsolve(A, b)
-        elapsed = time.time()-t
+        te = time.time()
+        tElapsed = te-ts
         
         #Calcolo dell'errore
         relErr = numpy.linalg.norm(x-xe)/numpy.linalg.norm(xe)
@@ -126,7 +133,7 @@ def mainComput(matrices,matricesInfos,firstTime,secondErrors,thirdRAM):
         usedMem = numpy.abs(usedMem)
         
         #Appendo valori
-        firstTime.append([sizeM,elapsed])
+        firstTime.append([sizeM,tElapsed])
         secondErrors.append([sizeM,relErr])
         thirdRAM.append([sizeM,usedMem])
     
