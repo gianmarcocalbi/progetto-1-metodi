ff22ad60244ae3ad648e4b25dd26bdda9cf7fc0a
grimidev <calbigianmarco@gmail.com>
2017-06-13 11:26:49 +0200

    Converted indentation from 2 spaces to 4

--- julia/solveMatrix.jl
+++ julia/solveMatrix.jl
@@ -3,71 +3,71 @@ using SuiteSparse
 using BenchmarkTools
 function solveMatrix(x...)
 
-  #Creo cellArray
-  matrices = Array{Any}(8)
+    #Creo cellArray
+    matrices = Array{Any}(8)
 
-  #Array per i risultati
-  allTimes = []
-  allErrors = []
-  allSizes = []
-  allMems = []
+    #Array per i risultati
+    allTimes = []
+    allErrors = []
+    allSizes = []
+    allMems = []
 
-  #Carico le matrici FEMLAB nel cellArray
-  matrices[1] = MatrixMarket.mmread("../matrices/mtx/ns3Da.mtx")
-  matrices[2] = MatrixMarket.mmread("../matrices/mtx/poisson2D.mtx")
-  matrices[3] = MatrixMarket.mmread("../matrices/mtx/poisson3Da.mtx")
-  matrices[4] = MatrixMarket.mmread("../matrices/mtx/poisson3Db.mtx")
-  matrices[5] = MatrixMarket.mmread("../matrices/mtx/problem1.mtx")
-  matrices[6] = MatrixMarket.mmread("../matrices/mtx/sme3Da.mtx")
-  matrices[7] = MatrixMarket.mmread("../matrices/mtx/sme3Db.mtx")
-  matrices[8] = MatrixMarket.mmread("../matrices/mtx/sme3Dc.mtx")
+    #Carico le matrici FEMLAB nel cellArray
+    matrices[1] = MatrixMarket.mmread("../matrices/mtx/ns3Da.mtx")
+    matrices[2] = MatrixMarket.mmread("../matrices/mtx/poisson2D.mtx")
+    matrices[3] = MatrixMarket.mmread("../matrices/mtx/poisson3Da.mtx")
+    matrices[4] = MatrixMarket.mmread("../matrices/mtx/poisson3Db.mtx")
+    matrices[5] = MatrixMarket.mmread("../matrices/mtx/problem1.mtx")
+    matrices[6] = MatrixMarket.mmread("../matrices/mtx/sme3Da.mtx")
+    matrices[7] = MatrixMarket.mmread("../matrices/mtx/sme3Db.mtx")
+    matrices[8] = MatrixMarket.mmread("../matrices/mtx/sme3Dc.mtx")
 
-  #Calcola quanti parametri in input ci sono
-  howManyOptional = length(x)
+    #Calcola quanti parametri in input ci sono
+    howManyOptional = length(x)
 
-  #Matrici in input - se ce ne sono
-  if howManyOptional>0
-    inputMatrices = Array{Any}(howManyOptional)
-    for ll=1 : howManyOptional
-      inputMatrices[ll] = MatrixMarket.mmread(x[ll])
+    #Matrici in input - se ce ne sono
+    if howManyOptional>0
+        inputMatrices = Array{Any}(howManyOptional)
+        for ll=1 : howManyOptional
+            inputMatrices[ll] = MatrixMarket.mmread(x[ll])
+        end
     end
-  end
 
-  #Codice da eseguire se ci sono matrici in input
-  if (howManyOptional>0)
-    (allMems, allTimes, allErrors, allSizes) = interCalc(inputMatrices, allMems, allTimes, allErrors, allSizes)
-  else
-    (allMems, allTimes, allErrors, allSizes) = interCalc(matrices, allMems, allTimes, allErrors, allSizes)
-  end
+    #Codice da eseguire se ci sono matrici in input
+    if (howManyOptional>0)
+        (allMems, allTimes, allErrors, allSizes) = interCalc(inputMatrices, allMems, allTimes, allErrors, allSizes)
+    else
+        (allMems, allTimes, allErrors, allSizes) = interCalc(matrices, allMems, allTimes, allErrors, allSizes)
+    end
 
-  #Lunghezza
-  if howManyOptional>0
-    numDiMatrici = howManyOptional;
-  else
-    numDiMatrici = 8;
-  end
+    #Lunghezza
+    if howManyOptional>0
+        numDiMatrici = howManyOptional;
+    else
+        numDiMatrici = 8;
+    end
 
-  #Creo matrici contenti i valori dei grafici
-  #Ordino anche in modo crescento rispetto alla size
-  firstTime = [allSizes ; allTimes];
-  firstTime = reshape(firstTime,numDiMatrici,2);
-  firstTimeSor = sortrows(firstTime,by=y->(y[1]))
-  firstTimeSor = reshape(firstTimeSor,numDiMatrici,2);
-  #writedlm("C:\\Users\\Nico\\Dropbox\\ProgettoMETODI\\solveMatrix_Julia\\tempistiche.txt", firstTimeSor, ',');
+    #Creo matrici contenti i valori dei grafici
+    #Ordino anche in modo crescento rispetto alla size
+    firstTime = [allSizes ; allTimes];
+    firstTime = reshape(firstTime,numDiMatrici,2);
+    firstTimeSor = sortrows(firstTime,by=y->(y[1]))
+    firstTimeSor = reshape(firstTimeSor,numDiMatrici,2);
+    #writedlm("C:\\Users\\Nico\\Dropbox\\ProgettoMETODI\\solveMatrix_Julia\\tempistiche.txt", firstTimeSor, ',');
 
-  secondErrors = [allSizes ; allErrors];
-  secondErrors = reshape(secondErrors,numDiMatrici,2);
-  secondErrorsSor = sortrows(secondErrors,by=y->(y[1]))
-  secondErrorsSor = reshape(secondErrorsSor,numDiMatrici,2);
-  #writedlm("C:\\Users\\Nico\\Dropbox\\ProgettoMETODI\\solveMatrix_Julia\\errori.txt", secondErrorsSor, ',');
+    secondErrors = [allSizes ; allErrors];
+    secondErrors = reshape(secondErrors,numDiMatrici,2);
+    secondErrorsSor = sortrows(secondErrors,by=y->(y[1]))
+    secondErrorsSor = reshape(secondErrorsSor,numDiMatrici,2);
+    #writedlm("C:\\Users\\Nico\\Dropbox\\ProgettoMETODI\\solveMatrix_Julia\\errori.txt", secondErrorsSor, ',');
 
-  thirdRAM = [allSizes ; allMems];
-  thirdRAM = reshape(thirdRAM,numDiMatrici,2);
-  thirdRAMSor = sortrows(thirdRAM,by=y->(y[1]));
-  thirdRAMSor = reshape(thirdRAMSor,numDiMatrici,2);
-  #writedlm("C:\\Users\\Nico\\Dropbox\\ProgettoMETODI\\solveMatrix_Julia\\allocatedBytes.txt", thirdRAMSor, ',');
+    thirdRAM = [allSizes ; allMems];
+    thirdRAM = reshape(thirdRAM,numDiMatrici,2);
+    thirdRAMSor = sortrows(thirdRAM,by=y->(y[1]));
+    thirdRAMSor = reshape(thirdRAMSor,numDiMatrici,2);
+    #writedlm("C:\\Users\\Nico\\Dropbox\\ProgettoMETODI\\solveMatrix_Julia\\allocatedBytes.txt", thirdRAMSor, ',');
 
-  return firstTimeSor, secondErrorsSor, thirdRAMSor;
+    return firstTimeSor, secondErrorsSor, thirdRAMSor;
 
 end
 
@@ -81,37 +81,37 @@ end
 
 
 function interCalc(matrices, allMems, allTimes, allErrors, allSizes)
-   for o=1 : size(matrices,1)
-    gc(); workspace();
-    tic();
+     for o=1 : size(matrices,1)
+        gc(); workspace();
+        tic();
 
-    #Salvo la soluzione esatta xe del sistema
-    xe = ones(size(matrices[o],1));
+        #Salvo la soluzione esatta xe del sistema
+        xe = ones(size(matrices[o],1));
 
-    #Calcolo del right hand side
-    b = matrices[o]*xe;
+        #Calcolo del right hand side
+        b = matrices[o]*xe;
 
-    #Risoluzione
-    profiling = @timed(matrices[o]\b);
-    profiling = profiling[3];
-    x = matrices[o]\b;
-    print(size(matrices[o],1));
-    print("fatto\n");
+        #Risoluzione
+        profiling = @timed(matrices[o]\b);
+        profiling = profiling[3];
+        x = matrices[o]\b;
+        print(size(matrices[o],1));
+        print("fatto\n");
 
-    #Errore relativo
-    errRel = norm(x-xe)/norm(xe);
+        #Errore relativo
+        errRel = norm(x-xe)/norm(xe);
 
-    #Salvo dati utili
-    elapsed = toc();
-    allTimes = [allTimes ; elapsed];
-    allSizes = [allSizes ; size(matrices[o],1)];
-    allErrors = [allErrors ; errRel];
-    #allMems = [allMems];
-    allMems = [allMems ; profiling];
+        #Salvo dati utili
+        elapsed = toc();
+        allTimes = [allTimes ; elapsed];
+        allSizes = [allSizes ; size(matrices[o],1)];
+        allErrors = [allErrors ; errRel];
+        #allMems = [allMems];
+        allMems = [allMems ; profiling];
 
 
-  end
+    end
 
-  return allMems, allTimes, allErrors, allSizes;
+    return allMems, allTimes, allErrors, allSizes;
 
 end
